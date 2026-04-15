80ed30b1d9b11155847cf7157319afd4b9be79db
Fortstar09 <ogunyafort@gmail.com>
2026-04-15 19:59:58 +0100

    fix: vercel build

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === "OPTIONS") {
+  if (req.method === "GET") {
     return res.status(200).end();
   }
 
